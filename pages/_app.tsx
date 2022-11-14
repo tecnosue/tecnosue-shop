7243912f7889a4ac80c172a96e93f5e51d0763a7
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-11-15 00:39:20 +0100

    instalar material UI, crear temas y la base de datos.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {CssBaseline, ThemeProvider} from '@mui/material'
+import { lightTheme } from '../themes/light-theme';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={ lightTheme }>
+      <Component {...pageProps} />
+      <CssBaseline />
+
+    </ThemeProvider>
+    )
 }
