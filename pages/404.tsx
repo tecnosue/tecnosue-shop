90a7e0d4bb3b10d047bedd54dad5c02bf78c74a0
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-11-16 16:39:47 +0100

    pagina de inicio temporal y arreglar responsive de la pagina 404

--- pages/404.tsx
+++ pages/404.tsx
@@ -8,7 +8,7 @@ const Custom404 = () => {
   return (
 
     <ShopLayout title='Page not Found' pageDescription='Nada por aquí'>
-        <Box display='flex' justifyContent='center' alignItems='center' height='cal(100vh - 200px)' >
+        <Box display='flex' justifyContent='center' alignItems='center' height='cal(100vh - 200px)' sx={{ flexDirection: { xs: 'column', sm: 'row' }}} >
             <Typography variant='h1' component='h1' fontSize={80} fontWeight={200}>404 |</Typography>
             <Typography marginLeft={2}>No hay ningún producto aquí</Typography>
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
-import { Typography } from '@mui/material';
+import { Grid, Typography,Card, CardMedia, CardActionArea } from '@mui/material';
 import { ShopLayout } from '../components/layouts';
+import { initialData } from '../database/products';
+
 
 
 export default function Home() {
@@ -8,7 +10,30 @@ export default function Home() {
       <Typography variant='h1' component='h1'>mi-tienda</Typography>
       <Typography variant='h2' sx={{ mb: 1 }}>Todos los productos</Typography>
 
-      
+      <Grid container spacing={4}>{
+        initialData.products.map( product => (
+          <Grid item 
+            xs={6} 
+            sm={ 4 }
+            key={product.slug} >
+            <Card>
+              <CardActionArea>
+                <CardMedia 
+                  component='img'               
+                  image={ `products/${product.images[0]}` }
+                  alt={ product.title }
+                />
+
+              </CardActionArea >
+            </Card>                 
+          </Grid>
+        ))
+
+      }
+          
+            
+  
+      </Grid>
         
     </ShopLayout>
   )
