a5cdc0cbd50643ae41368e566c3fc23996cd6259
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-11-21 22:17:11 +0100

    end point busqueda

--- pages/api/products/index.ts
+++ pages/api/products/index.ts
@@ -1,39 +1,42 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { db, SHOP_CONSTANTS } from "../../../database";
-import { Product } from "../../../models";
-import { IProduct } from "../../../interfaces/products";
+import type { NextApiRequest, NextApiResponse } from 'next'
+import { db, SHOP_CONSTANTS } from '../../../database'
+import { Product } from '../../../models'
+import { IProduct } from '../../../interfaces/products';
 
-type Data = { message: string } | IProduct[];
+type Data = 
+| { message: string }
+| IProduct[]
 
-export default function handler(
-  req: NextApiRequest,
-  res: NextApiResponse<Data>
-) {
-  switch (req.method) {
-    case "GET":
-      return getProducts(req, res);
+export default function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
 
-    default:
-      return res.status(400).json({
-        message: "Bad request",
-      });
-  }
+    switch( req.method ) {
+        case 'GET':
+            return getProducts( req, res )
+
+        default:
+            return res.status(400).json({
+                message: 'Bad request'
+            })
+    }
 }
-// automaticamente se crea una funcion tradicional, pero da igual con una funcion de Flecha.
 
-const getProducts = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
-  const { gender = "all" } = req.query;
-  let condition = {};
-  if (gender !== "all" && SHOP_CONSTANTS.validGenders.includes(`${gender}`)) {
-    condition = { gender };
-  }
+const getProducts = async(req: NextApiRequest, res: NextApiResponse<Data>) => {
+    
+    const { gender = 'all' } = req.query;
 
-  await db.connect();
-  const products = await Product.find(condition)
-    .select("title images price inStock slug -_id")
-    .lean();
+    let condition = {};
 
-  await db.disconnect();
+    if ( gender !== 'all' && SHOP_CONSTANTS.validGenders.includes(`${gender}`) ) {
+        condition = { gender };
+    }
 
-  return res.status(200).json(products);
-};
+    await db.connect();
+    const products = await Product.find(condition)
+                                .select('title images price inStock slug -_id')
+                                .lean();
+
+    await db.disconnect();
+
+    return res.status(200).json( products );
+
+}
