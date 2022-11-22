28e10cfd9a71138ecd7faaccb8901835b7008211
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-11-22 20:52:27 +0100

    carga de imagenes a la vez que el texto

--- components/products/ProductCard.tsx
+++ components/products/ProductCard.tsx
@@ -17,6 +17,8 @@ interface Props {
 
 export const ProductCard: FC<Props> = ({ product }) => {
   const [isHovered, setIsHovered] = useState(false);
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
 
   const productImage = useMemo(() => {
     return isHovered
@@ -41,13 +43,14 @@ export const ProductCard: FC<Props> = ({ product }) => {
                 className="fadeIn"
                 image={productImage}
                 alt={product.title}
+                onLoad={ () => setIsImageLoaded(true)}
               />
             </CardActionArea>
           </Link>
         </NextLink>
       </Card>
 
-      <Box sx={{ mt: 1 }} className="fadeIn">
+      <Box sx={{ mt: 1, display: isImageLoaded ? 'block' : 'none'}} className="fadeIn">
         <Typography fontWeight={700}>{product.title}</Typography>
         <Typography fontWeight={500}>{`€${product.price}`}</Typography>
       </Box>
