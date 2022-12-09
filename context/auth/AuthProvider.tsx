121906c194b012683def529d2773d6cdf996e843
Susana Paracuellos <82464797+tecnosue@users.noreply.github.com>
2022-12-09 22:40:46 +0100

    autenticacion con github

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -95,6 +95,15 @@ export const AuthProvider:FC = ({ children }) => {
     const logout = () => {
         Cookies.remove('token');
         Cookies.remove('cart');
+        Cookies.remove('firstName');
+        Cookies.remove('lastName');
+        Cookies.remove('address');
+        Cookies.remove('address2');
+        Cookies.remove('zip');
+        Cookies.remove('city');
+        Cookies.remove('country');
+        Cookies.remove('phone');
+
         router.reload();
     }
  
